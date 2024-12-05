4b693a283a15369367ecde6ec039b2a3a1b2d2fb
Miguelmdcc <108490537+Miguelmdcc@users.noreply.github.com>
2024-12-05 09:19:13 -0300

    Second commit - jQuery

--- codigo.js
+++ codigo.js
@@ -1,14 +1,40 @@
-function enviar(){
-    var texto = document.getElementById("escrita").value;
-    if(texto){
-        document.getElementById("resultado").value = texto;
-        document.getElementById("escrita").value = "";
-    }else{
-        window.alert("Inserir dados");
-    }
-}
+// function enviar(){
+//     var texto = document.getElementById("escrita").value;
+//     if(texto){
+//         document.getElementById("resultado").style.display = "inline";
+//         document.getElementById("resultado").value = texto;
+//         document.getElementById("escrita").value = "";
+//     }else{
+//         window.alert("Inserir dados");
+//     }
+// }
 
-function resetar(){
-    document.getElementById("resultado").value = "";
-    document.getElementById("escrita").value = "";
-}
+// function resetar(){
+//     document.getElementById("resultado").style.display = "none";
+//     document.getElementById("resultado").value = "";
+//     document.getElementById("escrita").value = "";
+// }
+
+
+
+$(document).ready(function(){
+    $("#resultado").hide();
+    // Envia valor para o input que aparece em um fade-in
+    $("#envia").click(function(){
+      // $("#resultado").fadeIn();
+      var texto = $("#escrita").val();
+      if(texto){
+        $("#resultado").val(texto);
+        $("#resultado").slideDown();
+        $("#escrita").val("");
+      }else{
+        window.alert("Insira os dados!");
+      }
+    });
+
+    $("#reset").click(function(){
+      // $("#resultado").val("");
+      $("#resultado").fadeOut();
+    });
+  
+  });
